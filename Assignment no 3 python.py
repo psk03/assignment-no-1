4d6be3a37a703d6723e691332fe66554246a577e
Pranav <106670030+psk03@users.noreply.github.com>
2022-06-08 20:38:12 +0530

    Add files via upload

---
+++ Assignment no 3 python.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Ans1)Function advantageous to have in our program is that Functions reduce the need of Duplicate code.
+# This makes program shorter,easier to read and easier to update.The function can be reused again and again.This helps to reduce time to type a program.
+
+# Ans2)A Function is a set of code that performs a specific task and can be used whenever needed just by calling it. 
+
+# Ans3)The def statement creates a function.
+# Syntax for Function:-
+#   def function_name(parameters):
+# 	"""docstring"""
+# 	statement(s)
+# 
+
+# Ans4)A function is a procedure to achieve a particular result.
+# While Function call is using this function to achieve that task. Using a function to do a particular task any point in program is called as function call.
+
+# An5)There is one global scope and one local scope is created whenever a function is called.
+
+# Ans6)When a fuction is return,the local is destroyed and all variable in it are forgotten.
+
+#  Ans7)A return value is the value that function call also evaluates.Like any value, a return value can be used as a part of an expression.
+
+# Ans8)If the function doesnot have a return statement then its return value is None.
+
+# Ans9)A global statement will force a variable in a function to refer to a global variable.If you want to refer to a global variable in a function,you can use the global keyword to declare which variable are global.
+
+# Ans10)The data type of None is NoneType.
+
+# Ans11)The import statement imports a module named areallyourpetsnamederic.
+
+# Ans12)The function can be called with spam.bacon().
+
+# Ans13) To save a programme from crashing if it encounters an error we have to place the line of code that might cause an error in a try clause and use except block to handle error.
+
+# Ans14):The code that could potentially cause an error goes in the try clause. The code that executes if an error happens goes in the except clause.
