3ffcc67cd019ae8cbb417e0dd68f48e7d2617a06
Pranav <106670030+psk03@users.noreply.github.com>
2022-06-11 14:40:25 +0530

    Add files via upload

---
+++ Assignment no 5 python.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Ans1)An empyty dictionary is represented by two empty curly bracket.
+# d = {} or d = dict{}.
+
+# Ans2)The value with key 'foo' and value 42 is{'foo':42}.
+
+# Ans3)The Dictionaries are represented by {} whereas as list are represented by [].
+# The items stored in a dictionary are unordered,while the items in a list are in ordered.
+
+# Ans4)IF we try to access spam['foo'] if spam with is{'bar':100}, we wll get a keyerror (KeyError : 'foo').
+
+# Ans5)There is no difference.The operator checks whether a value exist as key in a dictionary or not.
+
+# Ans6)The 'cat' in spam is used to check whether there is a 'cat' key in the dictionary.
+# While 'cat' in spam.values() checks whether there is a value 'cat' for one of the keys in spam.
+
+# Ans7)spam.setdefault('color','black').
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
